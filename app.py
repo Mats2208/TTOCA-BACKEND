abb92e38168f448f93b34891ea67a20c90a506dd
mats2208 <a2024111556@estudiantes.upsa.edu.bo>
2025-05-16 00:40:32 -0400

    Funcionamineto de /home

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from api.auth import auth_bp
+from api.empresa import empresa_bp
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}) 
 # Registrar el blueprint de autenticación
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
+
+#registro para commitear empresas desde /home
+app.register_blueprint(empresa_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
